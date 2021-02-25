e173598fe30e63c1d8a54c257ac6f7f811acf379
Danika Jaskulak-Gonzalez <danikaherminise@gmail.com>
2021-02-25 11:00:45 -0800

    OOP: dogs

---
+++ my_dogs.py
@@ -0,0 +1,14 @@
+# my_dogs.py
+import dog # we need to specify exactly what we want
+
+my_dog = dog.Dog("Moose", "Mr. Pee Pee Boy")
+my_dog.bark()
+
+my_other_dog = dog.Dog("Mila", "Ms. Poo Poo Girl")
+print(my_other_dog.name)
+
+annas_dog = dog.Dog("Spyder", "SpiderDog")
+print(annas_dog.name)
+
+annas_other_dog = dog.Dog("Teddy", "SausageGirl")
+print(annas_other_dog.name)
